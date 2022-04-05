1d74ea27f3124b4bf7210cedde29d3f9af2bd6bc
liaamHM <hm.liam.2002@gmail.com>
2022-04-05 12:36:24 +0200

    16

--- Programacion/UF4/Ejercicio2/MedinaLiamPersona.java
+++ Programacion/UF4/Ejercicio2/MedinaLiamPersona.java
@@ -4,6 +4,7 @@ package com.mycompany.keke;
 import java.util.Scanner;
 
 public class MedinaLiamPersona {
+    //creamos las variables necesaris donde se guardara de informacion de nuestro usuario
         int numeroDNI;
         char letraDNI;
         char sexo = 'O';
@@ -11,6 +12,8 @@ public class MedinaLiamPersona {
         double peso;
         int edad;
         String nombre;
+
+        //creamos un constructor para guardar la informacion en las variables necesarias
 
     public MedinaLiamPersona(int numeroDNI, char sexo, double altura, double peso, int edad, String nombre) {
         this.numeroDNI = numeroDNI;
@@ -21,19 +24,19 @@ public class MedinaLiamPersona {
         this.edad = edad;
         this.nombre = nombre;
     }
-
-    public MedinaLiamPersona() {
-    }
     
-    
-    public char letraNum(int num){
+    //creo la clase donde hare la operacion para calcular la letra del DNI del usuario
+    //esta operacion ya la hiciamos en otro ejercicio asi que no creo que sea neceasrio comentar esta clase
+    public void letraNum(int num){
         char letras[] = new char[]{'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
         int residuo = num % 23;
         char letra = letras[residuo];
         letraDNI = letra;
-        return letra;
     }
 
+    //en la siguiente clase calculamos el IMC
+    //de enrada introducimos la altura y el peso para poder hacer las operaciones mas adelnate
+    //de salida devolvemos -1/0/1, depende de el indice que haya obtneido el usuario
     public int IMC(double altura, double peso){
         double IMC;
         int respuesta;
@@ -53,6 +56,8 @@ public class MedinaLiamPersona {
         return respuesta;
     }
 
+    //en esta clase calculamos si la edad del usuario es mayor o igual a 18 para saber si es mayor de edad o no
+    //en esta clase la salida sera un boleano, ya que si es mayor de edad saldra true y si es menor saldra false, de esta manera sabremos si es mayor de edad o no
     public boolean legalidadEdad (){
         boolean respuesta;
         if (edad < 18){
@@ -64,7 +69,8 @@ public class MedinaLiamPersona {
         return respuesta;
     }
 
-
+    //por ultimo esta clase es una clase donde se imprime practicamnete toda la informacion que nos ha facilitado el usuario y 
+    //alguna que hemos conseguido nosotros a traves de operaciones
     public void informacionCompleta(){
         System.out.println("");
         System.out.println("INFORMACION COMPLETA:");
--- Programacion/UF4/Ejercicio2/MedinaLiamPersonaPrincipal.java
+++ Programacion/UF4/Ejercicio2/MedinaLiamPersonaPrincipal.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class MedinaLiamPersonaPrincipal {
 
     public static void main(String[] args) {
+        
+        //aqui como en el aterior archivo creo las variables para guardar la informacion del usuario y asi poder enviarlas al contructor en un momento dado
+
         int numeroDNI = 0;
         char sexo = 'O';
         double altura = 0;
@@ -12,9 +15,13 @@ public class MedinaLiamPersonaPrincipal {
         int edad = 0;
         String nombre = "";
 
+        //creo un scanner para preguntar el nombre al usuario
+
         Scanner entrada = new Scanner(System.in);
         System.out.println("¿Como te llamas?");
         boolean salir = false;
+
+        //hago una validacion de datos simple con bandera
 
         do {
             if (entrada.hasNextLine()) {
@@ -26,8 +33,12 @@ public class MedinaLiamPersonaPrincipal {
 
         } while (!salir);
 
+        //aqui pregunto la edad al usuario
+
         System.out.println("¿Cuantos años tienes?");
         boolean salir2 = false;
+
+        //vulevo a hacer una validacion simple con bandera para saber si el usuario ha introduciod un valor valido
 
         while (!salir2) {
             if (entrada.hasNextInt()) {
@@ -37,14 +48,18 @@ public class MedinaLiamPersonaPrincipal {
                 System.out.println("Error, introduce una edad valida");
             }
             entrada.nextLine();
+            //limpio el buffer para que no se cree un bucle con el while
         }
 
         boolean salir3 = false;
-
+        //esta vez pregunto dentro del bucle la numeracion del DNI ya que si el usuario se confunde prefiero que vuelva a salir
+        //la pregunta completa para que se de cuenta de lo que realmente estoy pidiendo 
         do {
             System.out.println("Introduce solamente la numeración de tu DNI:");
+            //valido si hay alguna letra en lo que ha introducido el usuario
             if (entrada.hasNextInt()) {
                 numeroDNI = entrada.nextInt();
+                //despues valido si el numero que ha introducido es valido y no supera los 100 millones o es negativo
                 if (numeroDNI < 99999999 && numeroDNI > 0) {
                     salir3 = true;
                 } else {
@@ -57,13 +72,17 @@ public class MedinaLiamPersonaPrincipal {
                 entrada.nextLine();
             }
             entrada.nextLine();
+            //antes de salir limpio el buffer para que no se haga un bucle infinito con el while
         } while (!salir3);
+
+        //pregunto al usuario cual es su sexo y le expongo las opciones
 
         System.out.println("¿Cual es tu sexo? (H/M/Otro)");
         boolean salir4 = false;
 
         while (!salir4) {
             if (entrada.hasNextLine()) {
+                //aqui pongo que si no es o hombre o mujer que el programa automaticamnete ponga la opcion de "Otro" 
                 sexo = entrada.nextLine().charAt(0);
                 if (sexo == 'H' || sexo == 'M') {
                     salir4 = true;
@@ -77,8 +96,12 @@ public class MedinaLiamPersonaPrincipal {
 
         }
 
+        //pregunto cuanto pesa al usuario
+
         System.out.println("¿Cuanto pesas?");
         boolean salir5 = false;
+
+        // validacion de dato con un bucle con bandera
 
         while (!salir5) {
             if (entrada.hasNextDouble()) {
@@ -87,8 +110,12 @@ public class MedinaLiamPersonaPrincipal {
             } else {
                 System.out.println("Error, introduce un numero valido");
             }
+            //limpio el buffer para que no se cree un bucle con el while y porque ademas el siguiente valor que tiene que
+            //introducir el usuario tambies es un double y no quier que se introduzca el mismo valor en la altura
             entrada.nextLine();
         }
+
+        //hago exactamente lo mismo que cuando pregunto el peso pero esta vez con la altrua
 
         System.out.println("¿Cuanto mides?");
         boolean salir6 = false;
@@ -103,10 +130,16 @@ public class MedinaLiamPersonaPrincipal {
             entrada.nextLine();
         }
 
+        //una vez tengo introducidos todos los datos se los envio al constructor para que este introduzca los datos en el otro .java
         MedinaLiamPersona p1 = new MedinaLiamPersona(numeroDNI, sexo, altura, peso, edad, nombre);
+        //pido al programa que me calcule la letra del numero de DNI
         p1.letraNum(numeroDNI);
+        //pido al programa que imprima la informacion completa del usuario ya que ya tengo todo lo que necesito para poder imprimir todo
         p1.informacionCompleta();
+        //pido al programa que me calcule el IMC con la clase que he creado y lo introduzco en una variable
         int IMC = p1.IMC(altura, peso);
+
+        //creo un switch para diferenciar que numero tengo y que es necesario imprimir en cada caso
         switch (IMC) {
             case -1:
                 System.out.println("Tu peso esta por debajo de lo normal.");
@@ -118,8 +151,10 @@ public class MedinaLiamPersonaPrincipal {
                 System.out.println("Tienes sobrepeso.");
                 break;
         }
+        //pido al programa que me detecte si la persona es mayor de edad y lo introduzco en una variabel boleana 
         boolean respuesta = p1.legalidadEdad();
 
+        //cre un if donde si el boleano es true imrpima que es mayor de edad y si no que imprima que es menor
         if (respuesta == true) {
             System.out.println("Eres mayor de edad.");
         } else {
